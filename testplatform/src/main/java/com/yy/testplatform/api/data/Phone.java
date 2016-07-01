8d44a71b5961eaa25a82e4d81c8b7d074b4c16c7
ChenShuai <boycs007@126.com>
2016-07-01 18:10:00 +0800

    test

---
+++ testplatform/src/main/java/com/yy/testplatform/api/data/Phone.java
@@ -0,0 +1,9 @@
+package com.yy.testplatform.api.data;
+
+import java.io.Serializable;
+
+public class Phone implements Serializable{
+
+	private static final long serialVersionUID = -6522855266746426890L;
+
+}
